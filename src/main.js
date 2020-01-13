a12a0037f7f15792e5907edb32a939edb579408d
Yasin Sütcü <yasinstc@gmail.com>
2020-01-13 18:27:55 +0300

    Prevent blank screen in Electron builds

--- src/main.js
+++ src/main.js
@@ -14,5 +14,9 @@ new Vue({
 	router,
 	store,
 	vuetify,
-	render: h => h(App)
+	render: h => h(App),
+	created() {
+		// Prevent blank screen in Electron builds
+		this.$router.push('/')
+	}
 }).$mount("#app");
